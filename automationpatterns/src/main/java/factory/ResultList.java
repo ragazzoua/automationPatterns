0fcc08e7bc9befcd8732d51d3ee5df5143401f42
Serhii <ragazzo@ukr.net>
2021-03-09 21:10:45 +0200

    factory added

--- automationpatterns/src/main/java/factory/ResultList.java
+++ automationpatterns/src/main/java/factory/ResultList.java
@@ -1,4 +1,11 @@
 package factory;
 
 public class ResultList extends Component{
+    public void checkResultHeadline() {
+        System.out.println("Check if the headline is correct.");
+    }
+
+    public void checkResults() {
+        System.out.println("Check if there are search results.");
+    }
 }
